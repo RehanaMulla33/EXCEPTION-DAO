de4abc1a439a6d31feb61ca2077859071c095c64
RehanaMulla33 <89864443+RehanaMulla33@users.noreply.github.com>
2021-12-07 22:59:45 +0530

    Add files via upload

---
+++ exception/InvalidBrand.java
@@ -0,0 +1,9 @@
+package com.xworkz.Assignment3.Exception.Helmet.exception;
+
+public class InvalidBrand extends RuntimeException{
+	public InvalidBrand(String msg) {
+		super(msg);
+	}
+	
+
+}
---
+++ exception/InvalidPrice.java
@@ -0,0 +1,9 @@
+package com.xworkz.Assignment3.Exception.Helmet.exception;
+
+public class InvalidPrice extends RuntimeException{
+
+	public InvalidPrice(String msg) {
+		super(msg);
+	}
+
+	}
